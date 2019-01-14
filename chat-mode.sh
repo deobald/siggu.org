a7ab6e73257004945073f89266895bfaa7ad5516
Steven Deobald <steven@deobald.ca>
2019-01-14 18:57:13 +0530

    saving the little chat mode script

---
+++ chat-mode.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+if [ $# -eq 0 ]
+  then
+    echo "Please specify 'on' or 'off'."
+    exit
+fi
+
+if [ "$1" = "on" ]
+  then
+    open /Applications/Telegram.app
+    open /Applications/LINE.app
+    open /Applications/WhatsApp.app
+    open /Applications/Signal.app
+    exit
+fi
+
+if [ "$1" = "off" ]
+  then
+    osascript -e 'quit app "Signal"'
+    osascript -e 'quit app "WhatsApp"'
+    osascript -e 'quit app "LINE"'
+    osascript -e 'quit app "Telegram"'
+    exit
+fi
+
+echo "Please specify 'on' or 'off'."
+exit
+
+
