b9b4cd8363f5552475b61d4c2b1496f9e35bf9b7
Steven Deobald <steven@deobald.ca>
2019-02-01 08:37:05 +0530

    changing chat-mode order so dark windows (telegram) are on top

--- chat-mode.sh
+++ chat-mode.sh
@@ -11,20 +11,20 @@ fi
 
 if [ "$1" = "on" ]
   then
-    open /Applications/Telegram.app
     open /Applications/LINE.app
     open /Applications/WhatsApp.app
     open /Applications/Signal.app
     open /Applications/Messages.app
+    open /Applications/Telegram.app
     exit
 fi
 
 if [ "$1" = "off" ]
 then
-    osascript -e 'quit app "Messages"'
-    osascript -e 'quit app "Signal"'
-    osascript -e 'quit app "WhatsApp"'
     osascript -e 'quit app "LINE"'
+    osascript -e 'quit app "WhatsApp"'
+    osascript -e 'quit app "Signal"'
+    osascript -e 'quit app "Messages"'
     osascript -e 'quit app "Telegram"'
     exit
 fi
